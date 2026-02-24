94d7b5e1db114fcb41bdc0909a3bfa99c7eff082
skirknefta <ltkien0105@gmail.com>
2026-02-24 23:41:59 +0800

    refactor: clean up main.py by removing commented-out code and unused imports

--- src/api/main.py
+++ src/api/main.py
@@ -4,37 +4,16 @@ from pathlib import Path
 
 # Add project root to path
 sys.path.insert(0, str(Path(__file__).parent.parent))
-from models.universe_meeps import UniverseMeeps
-from models.faction import Faction
-from models.content_panel import ContentPanel, ContentPanelCard
+from api.runeterra_map_api import RuneterraMapAPI
 
-from utils.file_io import write_json_from_pydantic_model
-from utils.riot_api import UniverseType, build_universe_url
-
-# response = requests.get(build_universe_url(UniverseType.SEARCH, language="en"))
-# write_json_from_pydantic_model(
-#     path="output/universe_meeps/overview.json", model=UniverseMeeps, response=result
-# )
-# response = dict(response.json())
-# universer_meeps = UniverseMeeps.model_validate(response)
-# faction_slugs = [
-#     build_universe_url(UniverseType.FACTION, slug=faction.slug)
-#     for faction in universer_meeps.factions
-# ]
-# for slug in faction_slugs:
-#     response = requests.get(slug)
-#     write_json_from_pydantic_model(
-#         path=f"output/universe_meeps/{slug.split('/')[-2]}_faction.json",
-#         model=Faction,
-#         response=response,
-#     )
+RuneterraMapAPI.fetch_universe_meeps()
 
 # URL = "https://map.leagueoflegends.com/loc/content-panels-en_us.json"
-URL = "https://map.leagueoflegends.com/loc/content-panel-cards-en_us.json"
-filename = URL.split("/")[-1]
-response = requests.get(URL)
-write_json_from_pydantic_model(
-    path=f"output/{filename}",
-    model=ContentPanelCard,
-    response=response,
-)
+# URL = "https://map.leagueoflegends.com/loc/content-panel-cards-en_us.json"
+# filename = URL.split("/")[-1]
+# response = requests.get(URL)
+# write_json_from_pydantic_model(
+#     path=f"output/{filename}",
+#     model=ContentPanelCard,
+#     response=response,
+# )
